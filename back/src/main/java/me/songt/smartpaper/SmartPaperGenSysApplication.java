53379bbf0c50b8aa3bd35584ac86208e2b32e080
Sarah <519524118@qq.com>
2017-03-27 00:26:21 +0800

    重建主类

---
+++ back/src/main/java/me/songt/smartpaper/SmartPaperGenSysApplication.java
@@ -0,0 +1,14 @@
+package me.songt.smartpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPaperGenSysApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SmartPaperGenSysApplication.class, args);
+    }
+}
